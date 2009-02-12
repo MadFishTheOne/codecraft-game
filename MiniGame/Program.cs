9ab35c966cbbb6368c10a02ce95a628eceb19126
Anton <Anton@44857f5e-1b17-11de-a958-c9a343b5b891>
2009-02-12 14:26:47 +0000

    This is the first import

---
+++ MiniGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MiniGame
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (MiniGame game = new MiniGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+

92422280345eecde559f1cfcbb322f8e52c87f31
madfish <madfish@44857f5e-1b17-11de-a958-c9a343b5b891>
2009-02-13 22:12:51 +0000

    сделал загрузку плагинов AI, и тестовый плагин включен в проект все интерфейсы вынесены в отдельную сборку

---
+++ trunk/DemoAI/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiniGameInterfaces;
+
+namespace DemoAI
+{
+    public class Main : IAI
+    {
+        int playerNumber;
+        IGame game;
+        #region IAI Members
+
+        public string Author
+        {
+            get { return "Mad Fish"; }
+        }
+
+        public string Description
+        {
+            get { return "Simple demo AI"; }
+        }
+
+        public void Init(int PlayerNumber, IGame Game)
+        {
+            playerNumber = PlayerNumber;
+            game = Game;
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < game.UnitsCount; i++)
+            {
+                //Try to control ALL units :) That's a joke - core will not allow this
+                //Only player's ships will be controlled
+                game.GetUnit(i).GoTo(new GamePoint(300, 200), false);
+                game.GetUnit(i).Shoot();
+            }
+        }
+
+        #endregion
+    }
+}

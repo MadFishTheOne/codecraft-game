3e5227dcf72f43ef20438613b00d5b1beb8e1df6
madfish <madfish@44857f5e-1b17-11de-a958-c9a343b5b891>
2009-02-15 14:17:36 +0000

    Релиз 1.0

---
+++ tags/MiniGame-1.0/MiniGame/Content/effects/EnvironmentEffect.fx
@@ -0,0 +1,64 @@
+uniform extern float4x4 ViewProj : WORLDVIEWPROJECTION;
+uniform extern texture tex;
+uniform float4 Color;
+uniform float2 Size ;
+#define BatchSize 240
+float4 Positions[BatchSize];
+#define CPlayers 6
+float3 PlayerColors[CPlayers];
+
+sampler textureSampler = sampler_state
+{
+    Texture=<tex>;
+    MipFilter=Linear;
+    MagFilter=Linear;
+    MinFilter=Linear;
+};
+struct VS_OUTPUT
+{
+    float4 Position : POSITION;
+    float2 TextureCoordinate : TEXCOORD0;
+    float4 Color:TEXCOORD1;
+};
+
+VS_OUTPUT Transform(
+    float4 Pos  : POSITION, 
+    float Index : PSIZE, 
+    float2 Text : TEXCOORD0 )
+{
+   VS_OUTPUT Out;
+	Pos.x*=Size.x*0.5;
+	Pos.y*=Size.y*0.5;
+	float4 currPos=Pos;
+	currPos.x=-Pos.x*cos(Positions[Index].z)-Pos.y*sin(Positions[Index].z)+Positions[Index].x;
+	currPos.y=+Pos.x*sin(Positions[Index].z)-Pos.y*cos(Positions[Index].z)+Positions[Index].y;	
+    currPos.z=-15000;
+    
+	Out.Position = mul(currPos,ViewProj);    
+    Out.TextureCoordinate =  Text;
+    Out.Color=float4(PlayerColors[(int)Positions[Index].w],1);
+    
+    return Out;
+}
+
+float4 PixelShader( VS_OUTPUT vsout ) : COLOR
+{
+   return tex2D(textureSampler, vsout.TextureCoordinate);	
+   
+
+}
+
+technique TransformTechnique
+{
+    pass P0
+    {
+        vertexShader = compile vs_2_0 Transform();
+        pixelShader = compile ps_2_0 PixelShader();
+        ZEnable = true;
+        ZWriteEnable = true;
+        AlphaBlendEnable = false;
+        AlphaTestEnable = true;
+        AlphaFunc = Greater;
+        AlphaRef = 200;
+    }
+}
---
+++ tags/MiniGame-1.0/MiniGameInterfaces/Interfaces.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniGameInterfaces
+{
+    public enum ShipTypes
+    {
+        Destroyer, Corvette, Cruiser
+    }
+    public struct GameVector
+    {
+        public float X;
+        public float Y;
+        public GameVector(float X, float Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public static GameVector operator +(GameVector pt1, GameVector pt2)
+        {
+            return new GameVector(pt1.X+pt2.X,pt1.Y+pt2.Y);
+        }
+        public static GameVector operator -(GameVector pt1, GameVector pt2)
+        {
+            return new GameVector(pt1.X-pt2.X,pt1.Y-pt2.Y);
+        }
+        public static GameVector operator *(GameVector pt1, float op2)
+        {
+            return new GameVector(pt1.X *op2, pt1.Y *op2);
+        }
+        public static GameVector operator /(GameVector pt1, float op2)
+        {
+            return new GameVector(pt1.X / op2, pt1.Y / op2);
+        }
+        public float Length()
+        {
+            return (float)Math.Sqrt(X*X+Y*Y);
+        }
+        public float LengthSquared()
+        {
+            return X * X + Y * Y;
+        }
+        public static GameVector Normalize(GameVector pt)
+        {
+            return pt / pt.Length();
+        }
+        public static float Dot(GameVector pt1, GameVector pt2)
+        {
+            return pt1.X * pt2.X + pt1.Y * pt2.Y;
+        }
+        public static float Cos(GameVector pt1, GameVector pt2)
+        {
+            return (float)(Dot(pt1, pt2) / Math.Sqrt(pt1.LengthSquared() * pt2.LengthSquared()));
+        }
+    }
+    public interface IUnit
+    {
+        #region Getting unit state
+        /// <summary>
+        /// unit name
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// position in the world. in pixels
+        /// </summary>
+        GameVector Position { get; }
+        /// <summary>
+        /// looking direction. unit vector
+        /// </summary>
+        GameVector Forward { get; }
+        /// <summary>
+        /// time to recharge gun in seconds
+        /// </summary>
+        float TimeToRecharge { get; }
+        /// <summary>
+        /// rotation angle in radians
+        /// </summary>
+        float RotationAngle { get; }
+        /// <summary>
+        /// player owner
+        /// </summary>
+        int PlayerOwner { get; }
+        /// <summary>
+        /// unit current hit points 
+        /// </summary>
+        float HP { get; }
+        #endregion
+        #region Getting unit characteristics
+        /// <summary>
+        /// get's ship type - cruiser, corvette or destroyer
+        /// </summary>
+        ShipTypes ShipType { get; }
+        /// <summary>
+        /// in pixels. X is a width, Y is a length
+        /// </summary>
+        GameVector Size { get; }
+        /// <summary>
+        /// in pixels per seconds
+        /// </summary>
+        float MaxSpeed { get; }
+        /// <summary>
+        /// in pixels per square seconds
+        /// </summary>
+        float MaxSpeedAcceleration { get; }
+        /// <summary>
+        /// in radians per second
+        /// </summary>
+        float MaxRotationSpeed { get; }
+        /// <summary>
+        /// in radians per square second
+        /// </summary>
+        float MaxRotationAcceleration { get; }
+        /// <summary>
+        /// gun delay in seconds
+        /// </summary>
+        float DelayTime { get; }
+        /// <summary>
+        /// gun damage in HP
+        /// </summary>
+        float Damage { get; }
+        #endregion
+        #region Controlling the unit
+        /// <summary>
+        /// accelerate unit by given amount
+        /// </summary>
+        void Accelerate(float amount);
+        /// <summary>
+        /// acceleration by max value
+        /// </summary>
+        void Accelerate();
+        /// <summary>
+        /// deacceleration by max value
+        /// </summary>
+        void DeAccelerate();
+        /// <summary>
+        /// set constant speed to move with it. unit will reach and hold this speed unitl new acceleration or setting speed command received
+        /// </summary>
+        void SetSpeed(float Speed);
+        /// <summary>
+        /// set rotation angle. unit will try to reach this angle with max acceleration and hold it.
+        /// </summary>
+        void SetAngle(float Angle);
+        /// <summary>
+        /// unit tries to shoot
+        /// </summary>
+        /// <returns> true if shoot was provided(if gun was recharged)</returns>
+        bool Shoot();
+        /// <summary>
+        /// makes unit to go to target location
+        /// </summary>
+        /// <param name="TargetLocation">location to go to</param>
+        /// <param name="Stop">true if unit must try to stop there</param>
+        void GoTo(GameVector TargetLocation, bool Stop);
+        /// <summary>
+        /// this damage goes to every unit that was in the blow radius at the blow starting time
+        /// </summary>
+        float BlowDamage { get; }
+        /// <summary>
+        /// this radius  is used to draw blow and to damage units with this blow
+        /// </summary>            
+        float BlowRadius { get; }
+
+        #endregion
+    }
+    public interface IShot
+    {
+        /// <summary>
+        /// position in the world
+        /// </summary>
+        GameVector Position { get; }
+        /// <summary>
+        /// velocity vector
+        /// </summary>
+        GameVector Direction { get; }
+    }
+    public interface IGame
+    {
+        /// <summary>
+        /// set displayed text
+        /// </summary>
+        void SetText(string Text);
+        /// <summary>
+        /// total number of units
+        /// </summary>
+        int UnitsCount { get; }
+        /// <summary>
+        /// get interface of a specific unit
+        /// </summary>
+        IUnit GetUnit(int Index);
+        /// <summary>
+        /// total number of shots
+        /// </summary>
+        int ShotsCount { get; }
+        /// <summary>
+        /// get interface of a specific shot
+        /// </summary>
+        IShot GetShot(int Index);
+        /// <summary>
+        /// gets near units and shots for specified position
+        /// </summary>
+        /// <param name="Position">position to specify</param>
+        /// <param name="Radius">near zone radius</param>
+        /// <param name="NearUnits">list of units in the near zone</param>
+        /// <param name="NearShots">list of shots in the near zone</param>
+        void GetNearUnits(GameVector Position, float Radius, out List<IUnit> NearUnits, out List<IShot> NearShots);        
+    }
+    public interface IAI
+    {
+        /// <summary>
+        /// get AI author
+        /// </summary>
+        string Author { get; }
+        /// <summary>
+        /// get AI description
+        /// </summary>
+        string Description { get; }
+        /// <summary>
+        /// init AI state, and allow it to access the world
+        /// </summary>
+        void Init(int PlayerNumber, IGame Game);
+        /// <summary>
+        /// think, make strategic decisions and control units
+        /// </summary>
+        void Update();
+    }
+}
+ 
